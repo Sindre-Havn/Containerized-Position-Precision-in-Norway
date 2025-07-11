2af8559dc65cfc848c1539e534150a165b440ab1
Statens Vegvesen <sindreh23@gmail.com>
2025-07-11 11:04:43 +0200

    Minor code cleanup in backend/dowloadfile.py

--- backend/downloadfile.py
+++ backend/downloadfile.py
@@ -1,34 +1,34 @@
 import requests
 import gzip
 import os
-folder = 'unzipped/'
+from pathlib import Path
 
 # Geth the Broadcast ephemeries file from the CDDIS server
 def lastned(day, year):
+    folder = Path('unzipped/')
     filename = f'BRD400DLR_S_{year}{day}0000_01D_MN.rnx.gz'
-
-    url = f'https://cddis.nasa.gov/archive/gnss/data/daily/{year}/brdc/{filename}'
-
-    if not os.path.isfile(folder+filename[:-3]):
-        r = requests.get(url)
-        c = r.content
-      
-        with open(filename, 'wb') as fd:
-            fd.write(c)
-
-        with open(filename,'rb') as fd:
-            gzip_fd = gzip.GzipFile(fileobj=fd)
-            gzip_fd = gzip_fd.read()
-        
-        os.remove(filename)
-
-        with open(folder+filename[:-3],'wb') as f:
-            f.write(gzip_fd)
-        
-        return os.path.join(folder, filename[:-3]) 
-    else:
+    if Path(folder / filename.stem).is_file():
         #print('File Exists')
-        return os.path.join(folder, filename[:-3]) 
+        return folder / filename.stem
+    
+    url = f'https://cddis.nasa.gov/archive/gnss/data/daily/{year}/brdc/{filename}'
+    r = requests.get(url)
+    c = r.content
+    
+    with open(filename, 'wb') as fd:
+        fd.write(c)
+
+
+    with open(filename,'rb') as fd:
+        gzip_fd = gzip.GzipFile(fileobj=fd)
+        gzip_fd = gzip_fd.read()
+    
+    os.remove(filename)
+    
+    with open(folder / filename.stem, 'wb') as f:
+        f.write(gzip_fd)
+    
+    return folder / filename.stem
 
 
 
